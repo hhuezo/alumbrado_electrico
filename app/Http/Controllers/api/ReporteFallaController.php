098287f759cc2738a4658509b0ee93425b555108
hhuezo <hugo.alex.huezo@gmail.com>
2023-12-02 00:29:43 -0600

    store reporte falla api

--- app/Http/Controllers/api/ReporteFallaController.php
+++ app/Http/Controllers/api/ReporteFallaController.php
@@ -6,11 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Models\catalogo\Departamento;
 use App\Models\catalogo\Distrito;
 use App\Models\catalogo\TipoFalla;
+use App\Models\control\ReporteFalla;
+use Exception;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Carbon;
 
 class ReporteFallaController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         //
@@ -21,7 +28,7 @@ class ReporteFallaController extends Controller
         $departamentos = Departamento::get();
         $tipos = TipoFalla::get();
 
-        $response = ["departamentos" => $departamentos, "tipos"=>$tipos];
+        $response = ["departamentos" => $departamentos, "tipos" => $tipos];
 
         return $response;
     }
@@ -29,9 +36,59 @@ class ReporteFallaController extends Controller
 
     public function getDistritos($id)
     {
-        $distritos = Distrito::where('departamento_id','=',$id)->get();
+        $distritos = Distrito::where('departamento_id', '=', $id)->get();
 
-        return ["distritos"=>$distritos];
+        return ["distritos" => $distritos];
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            // Obtener la fecha actual
+            $fechaActual = Carbon::now();
+
+            // Formatear la fecha en el formato "Y-m-d"
+            $fechaFormateada = $fechaActual->format('Y-m-d');
+
+
+            $reporte_falla = new ReporteFalla();
+            $reporte_falla->fecha = $fechaActual->format('Y-m-d');
+            $reporte_falla->distrito_id = $request->distrito_id;
+            $reporte_falla->tipo_falla_id = $request->tipo_falla_id;
+            $reporte_falla->descripcion = $request->descripcion;
+            $reporte_falla->latitud = $request->latitud;
+            $reporte_falla->longitud = $request->longitud;
+            $reporte_falla->telefono_contacto = $request->telefono_contacto;
+            $reporte_falla->nombre_contacto = $request->nombre_contacto;
+            $reporte_falla->estado_reporte_id = 1;
+            if ($request->imagen) {
+                $base64Image = $request->input('imagen'); // Obtener la cadena de base64 desde la solicitud
+                $decodedImage = base64_decode($base64Image); // Decodificar la cadena de base64
+
+                // Generar un nombre único para el archivo
+                $idFile = uniqid();
+                $nombre = $idFile  . '.png';
+
+                // Guardar la imagen en el directorio deseado
+                $ruta = public_path("docs/" . $nombre);
+                file_put_contents($ruta, $decodedImage);
+
+                $reporte_falla->url_foto = $nombre ;
+            }
+
+            $reporte_falla->save();
+
+            $response = ["value" => "1", "mensaje" => "Registro ingresado correctamente"];
+        } catch (Exception $e) {
+            $response = ["value" => "0", "mensaje" => "Error al procesar la solicitud"];
+        }
+
+        return $response;
+    }
+
+    public function show($id)
+    {
+        //
     }
 
     public function edit($id)
